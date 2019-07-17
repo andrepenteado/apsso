7d403c9e7ea4163eec6a2df8b9ead90f318fa70c
Andre Penteado <andre.penteado@unesp.br>
2019-07-17 16:56:35 -0300

    Docker composes complementados

--- src/main/resources/db/migration/V2019.07.05__Popular_Dados.sql
+++ src/main/resources/db/migration/V2019.07.05__Popular_Dados.sql
@@ -6,11 +6,11 @@ INSERT INTO Perfil_Usuario (Id_Usuario, Perfil) VALUES (LASTVAL(), 'Administrado
 INSERT INTO oauth_client_details
 (client_id, client_secret, scope, authorized_grant_types, web_server_redirect_uri, authorities,
  access_token_validity, refresh_token_validity, additional_information, autoapprove)
-VALUES ('com.gitlab.andrepenteado.clinic', '$2a$10$Pm7bLyuhA4Fhp4h0wmOKgebm8X892LGSJMdCGoI3oP8t/2hSMFvSm', 'user_info', 'authorization_code', 'http://localhost:30002/ap-clinic/login', null, 36000, 36000, null, true);
+VALUES ('com.gitlab.andrepenteado.apcontrole', '$2a$10$gGtZjuCrnFNK2L1f5Yj/BOBrXwsOYUSXZGn5Toz.2PKygvnID7Rxu', 'user_info', 'authorization_code', 'http://localhost:30001/ap-controle/login', null, 36000, 36000, null, true);
 
 INSERT INTO oauth_client_details
 (client_id, client_secret, scope, authorized_grant_types, web_server_redirect_uri, authorities,
  access_token_validity, refresh_token_validity, additional_information, autoapprove)
-VALUES ('com.gitlab.andrepenteado.controle', '$2a$10$qm2SDJrtELnkXfo2fKCGkOjuhZ5pV6G07B3b84CDs57W7cD7Kb/ga', 'user_info', 'authorization_code', 'http://localhost:30001/ap-controle/login', null, 36000, 36000, null, true);
+VALUES ('com.gitlab.andrepenteado.apclinic', '$2a$10$yDpuM/dxG8IePXRBcBq2k.kN7bvzrF7c8H7E5U7c49U4blNe0I/uq', 'user_info', 'authorization_code', 'http://localhost:30002/ap-clinic/login', null, 36000, 36000, null, true);
 
 ---------------------------------------------------------
