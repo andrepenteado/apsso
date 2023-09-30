646f3aebf7eef89e3c240c21697b674ed97109d1
André Penteado <andre.penteado.almeida@gmail.com>
2023-09-30 18:21:20 -0300

    Buscar usuário logado

--- backend/src/main/java/com/github/andrepenteado/apsso/backend/resources/AuthResource.java
+++ backend/src/main/java/com/github/andrepenteado/apsso/backend/resources/AuthResource.java
@@ -1,4 +1,4 @@
-package com.github.andrepenteado.apsso.login.resources;
+package com.github.andrepenteado.apsso.backend.resources;
 
 import com.github.andrepenteado.apsso.services.UsuarioService;
 import com.github.andrepenteado.apsso.services.models.Usuario;
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +23,9 @@ public class AuthResource {
     private final UsuarioService usuarioService;
 
     @GetMapping("/usuario")
-    public Usuario usuario(Authentication authentication) {
+    public Usuario usuario(Authentication authentication/*, @AuthenticationPrincipal OidcUser user*/) {
         String username = authentication.getName();
+        log.info("Buscar usuário logado " + username);
         Usuario usuario = usuarioService.buscar(username).get();
         return usuario;
     }
