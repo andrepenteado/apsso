28c441e9ef594d85bd1f404cfc90e580d29d0e22
André Penteado <andre.penteado@unesp.br>
2023-11-16 15:40:25 -0300

    Uso de ambientes do spring ao invés de variáveis de ambiente

--- controle/src/main/resources/db/migration/V2019.07.05__Popular_Dados.sql
+++ controle/src/main/resources/db/migration/V2019.07.05__Popular_Dados.sql
@@ -1,29 +1,3 @@
--- Desenvolvimento
-INSERT INTO public.oauth2_registered_client (
-  id, client_name, data_cadastro, url_entrada, client_id, client_id_issued_at, client_secret, client_secret_expires_at, client_authentication_methods,
-  authorization_grant_types, redirect_uris, post_logout_redirect_uris, scopes, client_settings, token_settings)
-VALUES (
-  'Controle-DEV', 'Módulo de cadastro de usuários e sistemas (MODO DESENVOLVIMENTO)', now(), 'http://dev.controle.apcode.com.br:30001', 'DEV.com.github.andrepenteado.apsso.controle', '2023-01-01 00:00:00.000000', '{bcrypt}$2a$10$xG.KPzsgZddwndKL9AQWquv1FdQW232DWRcC2GSLtr34aUaEEUOOa',
-  null, 'client_secret_basic',  'refresh_token,client_credentials,authorization_code', 'http://dev.controle.apcode.com.br:30001/authorized,http://dev.controle.apcode.com.br:30001/login/oauth2/code/controle-oidc',
-  'http://dev.controle.apcode.com.br:30001/logout', 'openid', '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}',
-  '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":false,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",900.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",86400.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",300.000000000],"settings.token.device-code-time-to-live":["java.time.Duration",300.000000000]}'
-);
-INSERT INTO perfil_sistema (authority, id_oauth2_registered_client, descricao)
-  VALUES ('ROLE_Controle-DEV_ARQUITETO', 'Controle-DEV', 'Arquiteto do Sistema');
-
-INSERT INTO public.oauth2_registered_client (
-    id, client_name, data_cadastro, url_entrada, client_id, client_id_issued_at, client_secret, client_secret_expires_at, client_authentication_methods,
-    authorization_grant_types, redirect_uris, post_logout_redirect_uris, scopes, client_settings, token_settings)
-VALUES (
-  'Portal-DEV', 'Portal de Sistemas (MODO DESENVOLVIMENTO)', now(), 'http://dev.portal.apcode.com.br:30002', 'DEV.com.github.andrepenteado.apsso.portal', '2023-01-01 00:00:00.000000', '{bcrypt}$2a$10$iJMyj3siGWVf1OARieTkAeynUscGkapD9Giu/PjkEVnKod/0PF.dC',
-  null, 'client_secret_basic',  'refresh_token,client_credentials,authorization_code', 'http://dev.portal.apcode.com.br:30002/authorized,http://dev.portal.apcode.com.br:30002/login/oauth2/code/portal-oidc',
-  'http://dev.portal.apcode.com.br:30002/logout', 'openid', '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}',
-  '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":false,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",900.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",86400.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",300.000000000],"settings.token.device-code-time-to-live":["java.time.Duration",300.000000000]}'
-);
-INSERT INTO perfil_sistema (authority, id_oauth2_registered_client, descricao)
-VALUES ('ROLE_Portal-DEV_USUARIO', 'Portal-DEV', 'Usuário de Sistemas');
-
--- Produção
 INSERT INTO public.oauth2_registered_client (
     id, client_name, data_cadastro, url_entrada, client_id, client_id_issued_at, client_secret, client_secret_expires_at, client_authentication_methods,
     authorization_grant_types, redirect_uris, post_logout_redirect_uris, scopes, client_settings, token_settings)
@@ -47,6 +21,18 @@ VALUES (
 );
 INSERT INTO perfil_sistema (authority, id_oauth2_registered_client, descricao)
 VALUES ('ROLE_Portal_USUARIO', 'Portal', 'Usuário de Sistemas');
+
+INSERT INTO public.oauth2_registered_client (
+    id, client_name, data_cadastro, url_entrada, client_id, client_id_issued_at, client_secret, client_secret_expires_at, client_authentication_methods,
+    authorization_grant_types, redirect_uris, post_logout_redirect_uris, scopes, client_settings, token_settings)
+VALUES (
+           'AProove', 'Sistema de pacientes e prontuários', now(), 'https://aproove.apcode.com.br', 'com.github.andrepenteado.aproove', '2023-01-01 00:00:00.000000', '{bcrypt}$2a$10$iJMyj3siGWVf1OARieTkAeynUscGkapD9Giu/PjkEVnKod/0PF.dC',
+           null, 'client_secret_basic',  'refresh_token,client_credentials,authorization_code', 'https://aproove.apcode.com.br/authorized,https://aproove.apcode.com.br/login/oauth2/code/aproove-oidc',
+           'https://aproove.apcode.com.br/logout', 'openid', '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}',
+           '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":false,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",900.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",86400.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",300.000000000],"settings.token.device-code-time-to-live":["java.time.Duration",300.000000000]}'
+       );
+INSERT INTO perfil_sistema (authority, id_oauth2_registered_client, descricao)
+VALUES ('ROLE_AProove_FISIOTERAPEUTA', 'AProove', 'Fisioterapeuta');
 
 INSERT INTO users (username, password, enabled, data_cadastro, nome) VALUES ('admin', '{bcrypt}$2a$10$DrggBKNTQujqeW2xPABOEuM1GgL.6VvdiZAP/hzChWxTj6TiWyLym', true, now(), 'Administrador');
 INSERT INTO authorities (username, authority) VALUES ('admin', 'ROLE_Controle-DEV_ARQUITETO');
