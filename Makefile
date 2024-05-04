6fcb231575009e8eff607a679fd26401ff42ab13
André Penteado <andre.penteado@unesp.br>
2024-05-04 19:08:31 -0300

    Adequado novos nomes das imagens docker

--- Makefile
+++ Makefile
@@ -6,14 +6,20 @@ build-all:
 	npm --prefix ./frontend/portal run build --omit=dev -- "-c=production"
 	mvn -U -f ./backend/pom.xml clean package -DskipTests
 	docker build -f .docker/Dockerfile.login.pipeline -t ghcr.io/andrepenteado/apsso/login -t ghcr.io/andrepenteado/apsso/login:$(VERSAO_APP) .
-	docker build -f .docker/Dockerfile.controle.pipeline -t ghcr.io/andrepenteado/apsso/controle -t ghcr.io/andrepenteado/apsso/controle:$(VERSAO_APP) .
-	docker build -f .docker/Dockerfile.portal.pipeline -t ghcr.io/andrepenteado/apsso/portal -t ghcr.io/andrepenteado/apsso/portal:$(VERSAO_APP) .
+	docker build -f .docker/Dockerfile.backend.controle.pipeline -t ghcr.io/andrepenteado/apsso/controle/backend -t ghcr.io/andrepenteado/apsso/controle/backend:$(VERSAO_APP) .
+	docker build -f .docker/Dockerfile.frontend.controle.pipeline -t ghcr.io/andrepenteado/apsso/controle/frontend -t ghcr.io/andrepenteado/apsso/controle/frontend:$(VERSAO_APP) .
+	docker build -f .docker/Dockerfile.backend.portal.pipeline -t ghcr.io/andrepenteado/apsso/portal/backend -t ghcr.io/andrepenteado/apsso/portal/backend:$(VERSAO_APP) .
+	docker build -f .docker/Dockerfile.frontend.portal.pipeline -t ghcr.io/andrepenteado/apsso/portal/frontend -t ghcr.io/andrepenteado/apsso/portal/frontend:$(VERSAO_APP) .
 	docker push ghcr.io/andrepenteado/apsso/login
 	docker push ghcr.io/andrepenteado/apsso/login:$(VERSAO_APP)
-	docker push ghcr.io/andrepenteado/apsso/controle
-	docker push ghcr.io/andrepenteado/apsso/controle:$(VERSAO_APP)
-	docker push ghcr.io/andrepenteado/apsso/portal
-	docker push ghcr.io/andrepenteado/apsso/portal:$(VERSAO_APP)
+	docker push ghcr.io/andrepenteado/apsso/controle/backend
+	docker push ghcr.io/andrepenteado/apsso/controle/backend:$(VERSAO_APP)
+	docker push ghcr.io/andrepenteado/apsso/controle/frontend
+	docker push ghcr.io/andrepenteado/apsso/controle/frontend:$(VERSAO_APP)
+	docker push ghcr.io/andrepenteado/apsso/portal/backend
+	docker push ghcr.io/andrepenteado/apsso/portal/backend:$(VERSAO_APP)
+	docker push ghcr.io/andrepenteado/apsso/portal/frontend
+	docker push ghcr.io/andrepenteado/apsso/portal/frontend:$(VERSAO_APP)
 	docker logout ghcr.io
 
 build-login:
@@ -28,18 +34,24 @@ build-controle:
 	echo $(GITHUB_TOKEN) | docker login ghcr.io --username andrepenteado --password-stdin
 	npm --prefix ./frontend/controle run build --omit=dev -- "-c=production"
 	mvn -U -f ./backend/pom.xml clean package --projects services,controle -DskipTests
-	docker build -f .docker/Dockerfile.controle.pipeline -t ghcr.io/andrepenteado/apsso/controle -t ghcr.io/andrepenteado/apsso/controle:$(VERSAO_APP) .
-	docker push ghcr.io/andrepenteado/apsso/controle
-	docker push ghcr.io/andrepenteado/apsso/controle:$(VERSAO_APP)
+	docker build -f .docker/Dockerfile.backend.controle.pipeline -t ghcr.io/andrepenteado/apsso/controle/backend -t ghcr.io/andrepenteado/apsso/controle/backend:$(VERSAO_APP) .
+	docker build -f .docker/Dockerfile.frontend.controle.pipeline -t ghcr.io/andrepenteado/apsso/controle/frontend -t ghcr.io/andrepenteado/apsso/controle/frontend:$(VERSAO_APP) .
+	docker push ghcr.io/andrepenteado/apsso/controle/backend
+	docker push ghcr.io/andrepenteado/apsso/controle/backend:$(VERSAO_APP)
+	docker push ghcr.io/andrepenteado/apsso/controle/frontend
+	docker push ghcr.io/andrepenteado/apsso/controle/frontend:$(VERSAO_APP)
 	docker logout ghcr.io
 
 build-portal:
 	echo $(GITHUB_TOKEN) | docker login ghcr.io --username andrepenteado --password-stdin
 	npm --prefix ./frontend/portal run build --omit=dev -- "-c=production"
 	mvn -U -f ./backend/pom.xml clean package --projects services,portal -DskipTests
-	docker build -f .docker/Dockerfile.portal.pipeline -t ghcr.io/andrepenteado/apsso/portal -t ghcr.io/andrepenteado/apsso/portal:$(VERSAO_APP) .
-	docker push ghcr.io/andrepenteado/apsso/portal
-	docker push ghcr.io/andrepenteado/apsso/portal:$(VERSAO_APP)
+	docker build -f .docker/Dockerfile.backend.portal.pipeline -t ghcr.io/andrepenteado/apsso/portal/backend -t ghcr.io/andrepenteado/apsso/portal/backend:$(VERSAO_APP) .
+	docker build -f .docker/Dockerfile.frontend.portal.pipeline -t ghcr.io/andrepenteado/apsso/portal/frontend -t ghcr.io/andrepenteado/apsso/portal/frontend:$(VERSAO_APP) .
+	docker push ghcr.io/andrepenteado/apsso/portal/backend
+	docker push ghcr.io/andrepenteado/apsso/portal/backend:$(VERSAO_APP)
+	docker push ghcr.io/andrepenteado/apsso/portal/frontend
+	docker push ghcr.io/andrepenteado/apsso/portal/frontend:$(VERSAO_APP)
 	docker logout ghcr.io
 
 start:
@@ -56,8 +68,10 @@ update:
 	$(MAKE) stop
 	docker image pull postgres:16
 	docker image pull ghcr.io/andrepenteado/apsso/login
-	docker image pull ghcr.io/andrepenteado/apsso/controle
-	docker image pull ghcr.io/andrepenteado/apsso/portal
+	docker image pull ghcr.io/andrepenteado/apsso/controle/backend
+	docker image pull ghcr.io/andrepenteado/apsso/controle/frontend
+	docker image pull ghcr.io/andrepenteado/apsso/portal/backend
+	docker image pull ghcr.io/andrepenteado/apsso/portal/frontend
 	docker logout ghcr.io
 	$(MAKE) start
 
--- Makefile.ps1
+++ Makefile.ps1
@@ -11,14 +11,20 @@ switch($exec) {
         $VERSAO = mvn help:evaluate '-Dexpression=project.version' '-q' '-DforceStdout' '-f' './backend/pom.xml'
         mvn -U clean package -DskipTests
         docker build -f .docker/Dockerfile.login.pipeline -t ghcr.io/andrepenteado/apsso/login -t ghcr.io/andrepenteado/apsso/login:$VERSAO .
-        docker build -f .docker/Dockerfile.controle.pipeline -t ghcr.io/andrepenteado/apsso/controle -t ghcr.io/andrepenteado/apsso/controle:$VERSAO .
-        docker build -f .docker/Dockerfile.portal.pipeline -t ghcr.io/andrepenteado/apsso/portal -t ghcr.io/andrepenteado/apsso/portal:$VERSAO .
+        docker build -f .docker/Dockerfile.backend.controle.pipeline -t ghcr.io/andrepenteado/apsso/controle/backend -t ghcr.io/andrepenteado/apsso/controle/backend:$VERSAO .
+        docker build -f .docker/Dockerfile.frontend.controle.pipeline -t ghcr.io/andrepenteado/apsso/controle/frontend -t ghcr.io/andrepenteado/apsso/controle/frontend:$VERSAO .
+        docker build -f .docker/Dockerfile.backend.portal.pipeline -t ghcr.io/andrepenteado/apsso/portal/backend -t ghcr.io/andrepenteado/apsso/portal/backend:$VERSAO .
+        docker build -f .docker/Dockerfile.frontend.portal.pipeline -t ghcr.io/andrepenteado/apsso/portal/frontend -t ghcr.io/andrepenteado/apsso/portal/frontend:$VERSAO .
         docker push ghcr.io/andrepenteado/apsso/login
         docker push ghcr.io/andrepenteado/apsso/login:$VERSAO
-        docker push ghcr.io/andrepenteado/apsso/controle
-        docker push ghcr.io/andrepenteado/apsso/controle:$VERSAO
-        docker push ghcr.io/andrepenteado/apsso/portal
-        docker push ghcr.io/andrepenteado/apsso/portal:$VERSAO
+        docker push ghcr.io/andrepenteado/apsso/controle/backend
+        docker push ghcr.io/andrepenteado/apsso/controle/backend:$VERSAO
+        docker push ghcr.io/andrepenteado/apsso/controle/frontend
+        docker push ghcr.io/andrepenteado/apsso/controle/frontend:$VERSAO
+        docker push ghcr.io/andrepenteado/apsso/portal/backend
+        docker push ghcr.io/andrepenteado/apsso/portal/backend:$VERSAO
+        docker push ghcr.io/andrepenteado/apsso/portal/frontend
+        docker push ghcr.io/andrepenteado/apsso/portal/frontend:$VERSAO
         docker logout ghcr.io
     }
     "build-login" {
@@ -34,20 +40,26 @@ switch($exec) {
         $VERSAO = mvn help:evaluate '-Dexpression=project.version' '-q' '-DforceStdout' '-f' './backend/pom.xml'
         npm --prefix ./frontend/controle run build --omit=dev -- "-c=production"
         mvn -U -f ./backend/pom.xml clean package --projects services,controle -DskipTests
-        docker build -f .docker/Dockerfile.controle.pipeline -t ghcr.io/andrepenteado/apsso/controle -t ghcr.io/andrepenteado/apsso/controle:$VERSAO .
+        docker build -f .docker/Dockerfile.backend.controle.pipeline -t ghcr.io/andrepenteado/apsso/controle/backend -t ghcr.io/andrepenteado/apsso/controle/backend:$VERSAO .
+        docker build -f .docker/Dockerfile.frontend.controle.pipeline -t ghcr.io/andrepenteado/apsso/controle/frontend -t ghcr.io/andrepenteado/apsso/controle/frontend:$VERSAO .
         Get-Content 'C:\Users\André Penteado\Documents\Particular\token-github.txt' | docker login ghcr.io --username andrepenteado --password-stdin
-        docker push ghcr.io/andrepenteado/apsso/controle
-        docker push ghcr.io/andrepenteado/apsso/controle:$VERSAO
+        docker push ghcr.io/andrepenteado/apsso/controle/backend
+        docker push ghcr.io/andrepenteado/apsso/controle/backend:$VERSAO
+        docker push ghcr.io/andrepenteado/apsso/controle/frontend
+        docker push ghcr.io/andrepenteado/apsso/controle/frontend:$VERSAO
         docker logout ghcr.io
     }
     "build-portal" {
         $VERSAO = mvn -f ./backend/pom.xml help:evaluate '-Dexpression=project.version' '-q' '-DforceStdout'
         npm --prefix ./frontend/portal run build --omit=dev -- "-c=production"
         mvn -U -f ./backend/pom.xml clean package --projects services,portal -DskipTests
-        docker build -f .docker/Dockerfile.portal.pipeline -t ghcr.io/andrepenteado/apsso/portal -t ghcr.io/andrepenteado/apsso/portal:$VERSAO .
+        docker build -f .docker/Dockerfile.backend.portal.pipeline -t ghcr.io/andrepenteado/apsso/portal/backend -t ghcr.io/andrepenteado/apsso/portal/backend:$VERSAO .
+        docker build -f .docker/Dockerfile.frontend.portal.pipeline -t ghcr.io/andrepenteado/apsso/portal/frontend -t ghcr.io/andrepenteado/apsso/portal/frontend:$VERSAO .
         Get-Content 'C:\Users\André Penteado\Documents\Particular\token-github.txt' | docker login ghcr.io --username andrepenteado --password-stdin
-        docker push ghcr.io/andrepenteado/apsso/portal
-        docker push ghcr.io/andrepenteado/apsso/portal:$VERSAO
+        docker push ghcr.io/andrepenteado/apsso/portal/backend
+        docker push ghcr.io/andrepenteado/apsso/portal/backend:$VERSAO
+        docker push ghcr.io/andrepenteado/apsso/portal/frontend
+        docker push ghcr.io/andrepenteado/apsso/portal/frontend:$VERSAO
         docker logout ghcr.io
     }
     "start" {
@@ -64,8 +76,10 @@ switch($exec) {
         Get-Content 'C:\Users\André Penteado\Documents\Particular\token-github.txt' | docker login ghcr.io --username andrepenteado --password-stdin
         docker image pull postgres:16
         docker image pull ghcr.io/andrepenteado/apsso/login
-        docker image pull ghcr.io/andrepenteado/apsso/controle
-        docker image pull ghcr.io/andrepenteado/apsso/portal
+        docker image pull ghcr.io/andrepenteado/apsso/controle/backend
+        docker image pull ghcr.io/andrepenteado/apsso/controle/frontend
+        docker image pull ghcr.io/andrepenteado/apsso/portal/backend
+        docker image pull ghcr.io/andrepenteado/apsso/portal/frontend
         docker logout ghcr.io
         docker compose -f .ansible/files/docker-compose.yml up -d
     }
