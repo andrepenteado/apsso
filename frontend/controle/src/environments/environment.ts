31fc7fed31f50147f848510225910553a21c94ec
André Penteado <andre.penteado@unesp.br>
2024-05-15 15:30:12 -0300

    Corrigidos manifestos de deploy em produção

--- frontend/controle/src/environments/environment.ts
+++ frontend/controle/src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
 
-  backendURL:  "https://api.controle.apcode.com.br",
+  backendURL:  "https://api-controle.apcode.com.br",
 
   portalURL: "https://portal.apcode.com.br",
 
-  backendPortalURL: "https://api.portal.apcode.com.br",
+  backendPortalURL: "https://api-portal.apcode.com.br",
 
   urlAuthorizationServer: "https://login.apcode.com.br",
 
